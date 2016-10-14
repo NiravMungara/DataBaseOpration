069fc7e3e64557610a003f5463501c66f3ebbc13
Nirav Mungara <d2ddeveloper@hotmail.com>
2016-10-14 14:43:31 +0530

    Commit confilicts

--- DataBaseOpration/App_Start/BundleConfig.cs
+++ DataBaseOpration/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace DataBaseOpration
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{versison}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
