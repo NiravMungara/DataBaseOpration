94e659495a4311edd4a50b449247e1891cc310a4
Nirav Mungara <d2ddeveloper@hotmail.com>
2016-10-14 13:36:18 +0530

    Test 1st Commit From VS

---
+++ DataBaseOpration/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DataBaseOpration
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
